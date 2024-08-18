e471a2b4efe8fd9bd3d07a19ae50572ecb61652d
bryanburch <bjburch7@gmail.com>
2024-08-17 18:18:45 -0700

    End of section 7

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,119 @@
+import { makeAutoObservable, runInAction } from "mobx";
+import { Activity } from "../models/activity";
+import agent from "../api/agent";
+import { v4 as uuid } from "uuid";
+
+export default class ActivityStore {
+    activityRegistry = new Map<string, Activity>();
+    selectedActivity: Activity | undefined = undefined;
+    editMode = false;
+    loading = false;
+    loadingInitial = true;
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    get activitiesByDate() {
+        return Array.from(this.activityRegistry.values()).sort((a, b) =>
+            Date.parse(a.date) - Date.parse(b.date))
+    }
+
+    loadActivities = async () => {
+        try {
+            const activities = await agent.Activities.list();
+            // If we're running synchronous alongside async,
+            // then we need to pass that sync code in runInAction().
+            runInAction(() => {
+                activities.forEach(activity => {
+                    activity.date = activity.date.split('T')[0];
+                    this.activityRegistry.set(activity.id, activity);
+                })
+                // this.activities = activities
+            })
+            // Didn't have to include in above runInAction scope since 
+            // setLoadingInitial is an action itself.
+            this.setLoadingInitial(false);
+        } catch (error) {
+            console.log(error);
+            this.setLoadingInitial(false);
+        }
+    }
+
+    // Created this action so we didn't have to wrap non-blocking 
+    // code in loadActivities() action with runInAction().
+    setLoadingInitial = (state: boolean) => {
+        this.loadingInitial = state;
+    }
+
+    selectActivity = (id: string) => {
+        this.selectedActivity = this.activityRegistry.get(id);
+    }
+
+    cancelSelectedActivity = () => {
+        this.selectedActivity = undefined;
+    }
+
+    openForm = (id?: string) => {
+        id ? this.selectActivity(id) : this.cancelSelectedActivity();
+        this.editMode = true;
+    }
+
+    closeForm = () => {
+        this.editMode = false;
+    }
+
+    createActivity = async (activity: Activity) => {
+        this.loading = true;
+        activity.id = uuid();
+        try {
+            await agent.Activities.create(activity);
+            runInAction(() => {
+                this.activityRegistry.set(activity.id, activity);
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
+    }
+
+    updateActivity = async (activity: Activity) => {
+        this.loading = true;
+        try {
+            await agent.Activities.update(activity);
+            runInAction(() => {
+                this.activityRegistry.set(activity.id, activity);
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
+    }
+
+    deleteActivity = async (id: string) => {
+        this.loading = true;
+        try {
+            await agent.Activities.delete(id);
+            runInAction(() => {
+                this.activityRegistry.delete(id);
+                if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
+                this.loading = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
+    }
+}
