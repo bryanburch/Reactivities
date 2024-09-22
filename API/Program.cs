2f8805a1619e4f65f99979d85cc5bb1fe1abf71e
bryanburch <bjburch7@gmail.com>
2024-09-22 10:49:10 -0700

    Add data to csp image sources

--- API/Program.cs
+++ API/Program.cs
@@ -36,7 +36,7 @@ app.UseCsp(opt => opt
     .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
     .FormActions(s => s.Self())
     .FrameAncestors(s => s.Self())
-    .ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com"))
+    .ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com", "data:"))
     .ScriptSources(s => s.Self())
 ); // use "UseCspReportOnly" for testing
 
